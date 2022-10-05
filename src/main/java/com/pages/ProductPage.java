b94fd5199b6dbcd1b8a5b3d2f3f61faf6a979246
Makesh <mageshbalraj@gmail.com>
2022-10-05 14:41:45 +0530

    git commit -m "SWAG-5 Added the validating the existence of all products"

--- src/main/java/com/pages/ProductPage.java
+++ src/main/java/com/pages/ProductPage.java
@@ -1,26 +1,76 @@
 package com.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.base.BaseClass;
+import com.utility.Gestures;
 
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class ProductPage extends BaseClass{
-	@AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Menu\"]/android.view.ViewGroup/android.widget.ImageView")
+public class ProductPage extends BaseClass
+{
+	@AndroidFindBy(xpath = "//android.widget.ScrollView[@content-desc=\"test-PRODUCTS\"]/android.view.ViewGroup/android.view.ViewGroup")
+	List<AndroidElement> allAvailableProducts;
+	@AndroidFindBy(accessibility = "test-Menu")
 	AndroidElement burgerButton;
 	
-	@AndroidFindBy(uiAutomator = "new UiSelector().text(\"LOGOUT\")")
+	@AndroidFindBy(accessibility = "test-LOGOUT")
 	AndroidElement logoutButton;
+	
+	@AndroidFindBy(accessibility = "test-BACK TO PRODUCTS")
+	AndroidElement backToProductsButton;
 	
 	public ProductPage() {
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
-	
+
 	public void logoutWithoutGestures() {
 		burgerButton.click();
 		logoutButton.click();
 	}
+
+	public String validatingTheExistenceOfAllProducts() {
+		int count=0;
+		try {
+		for(int i=0;i<allAvailableProducts.size();i++) {
+			for(int j=0;j<2;j++) {
+				allAvailableProducts.get(i).findElements(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-Item\"])")).get(j) .click();
+				backToProductsButton.click();
+				count++;
+			}
+//			Thread.sleep(3000);
+//			Gestures.scrollUsingDimension();
+		}
+		Thread.sleep(5000);
+		Gestures.scrollUsingDimension();
+		for(int i=0;i<allAvailableProducts.size()-1;) {
+			for(int j=0;j<2;j++) {
+				allAvailableProducts.get(i+1).findElements(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Item\"]")).get(j).click();
+				backToProductsButton.click();
+				count++;
+			}
+			break;
+		}
+		}
+		catch(Exception e) {
+			allAvailableProducts=driver.findElements(By.xpath("//android.widget.ScrollView[@content-desc=\\\"test-PRODUCTS\\\"]/android.view.ViewGroup/android.view.ViewGroup"));
+		}
+		System.out.println(count);
+		String validationMessage=null;
+		if(count>=6) {
+			validationMessage="All the elements are present and clickable";
+		}
+		else {
+			validationMessage="Only "+count +" elements were clicked";
+		}
+		return validationMessage;
+	}
+	
 }
--- src/main/java/com/utility/Gestures.java
+++ src/main/java/com/utility/Gestures.java
@@ -1,10 +1,16 @@
 package com.utility;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Dimension;
+
 import com.base.BaseClass;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class Gestures extends BaseClass{
 	static TouchAction touch=new TouchAction(driver);
@@ -15,5 +21,20 @@ public class Gestures extends BaseClass{
 	
 	public static void scrollingVertically(String androidElement) {
 		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\""+androidElement+"\" ))");
+	}
+	
+	
+	public static void scrollUsingDimension() {
+		Dimension dimension=driver.manage().window().getSize();
+		Double scrollStartHeightDouble=dimension.getHeight()*0.7;
+		int scrollStartHeight=scrollStartHeightDouble.intValue();
+		Double scrollEndHeightDouble=dimension.getHeight()*0.1;
+		int scrollEndHeight=scrollEndHeightDouble.intValue();
+		
+		new TouchAction(driver)
+		.press(PointOption.point(0,scrollStartHeight))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+		.moveTo(PointOption.point(0,scrollEndHeight))
+		.release().perform();
 	}
 }
